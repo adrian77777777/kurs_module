9677f5ee6026a14e5f2fac31f0f47b4ce98b5aca
adrian77777777 <90701570+adrian77777777@users.noreply.github.com>
2021-09-15 17:12:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,19 @@
 resource "aws_subnet" "delete-me-asap" {
-vpc_id = var.vpc
-cidr_block = var.cidr
-
- tags = {
-Name = var.subnet_name
+  vpc_id     = var.vpc
+  cidr_block = var.cidr
+  tags = {
+    Name = var.subnet_name
+  }
 }
-}
-
 variable "vpc" {
-type = string
-default = ""
+    type = string
+    default = ""
 }
-
 variable "cidr" {
-type = string
-default = ""
+    type = string
+    default = "10.10.0.16/28"
 }
-
 variable "subnet_name" {
-type = string
-default = ""
+    type = string
+    default = "just_some_name"
 }
